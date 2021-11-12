290635774ce69c1835ffd76e7be02b87abbdc64b
DeepakGanesh <57207206+deepak046@users.noreply.github.com>
2021-11-12 18:40:29 +0530

    Delete runBeamODE.m

--- runBeamODE.m
+++
@@ -1,9 +0,0 @@
-% Solving Beam ODE
-
-xlow = 0; xhigh = 1;
-xmesh = linspace(xlow,xhigh,10);
-solinit = bvpinit(xmesh,[0 1]);
-sol = bvp4c(@BeamODEfun, @bc, solinit);
-xint = linspace(xlow,xhigh);
-Sxint = deval(sol,xint);
-plot(xint, Sxint(1,:));
